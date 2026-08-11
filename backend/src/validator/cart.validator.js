9870d311c5dbea74d480687361fc2717eb4f6854
anand raj <git add README.md>
2026-08-11 20:19:05 +0530

    remove cart bug

--- backend/src/validator/cart.validator.js
+++ backend/src/validator/cart.validator.js
@@ -1,7 +1,7 @@
 import { body, validationResult } from 'express-validator';
 // ⚠️ Also added .js extensions here to prevent your previous ESM import error:
 import cartModel from '../models/cart.model.js';
-import productModel from '../models/product.model.js';
+import productModel from '../models/Product.model.js';
 
 export const cartValidator = [
     body('productid').custom(async (value, { req }) => {
